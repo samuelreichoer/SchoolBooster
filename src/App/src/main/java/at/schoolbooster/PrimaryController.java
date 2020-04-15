9811607d0b24930575456027aa63b8c13f1e8582
Niklas Strassl <40146720170043@litec.ac.at>
2020-04-16 00:00:05 +0200

    app changes

--- src/App/src/main/java/at/schoolbooster/PrimaryController.java
+++ src/App/src/main/java/at/schoolbooster/PrimaryController.java
@@ -1,60 +1,13 @@
 package at.schoolbooster;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.TextAlignment;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class PrimaryController {
-
-    @FXML private TabPane tabContainer;
-    @FXML private Tab timetableTab;
-    @FXML private Tab checklistTab;
-    @FXML private Tab datesTab;
-    @FXML private double tabWidth = 50.0;
 
     public PrimaryController() {
     }
 
     @FXML
-    private void initialize() throws FileNotFoundException {
-
+    private void initialize() {
     }
-
-    /*@FXML
-    public void configureTabPane() throws FileNotFoundException {
-
-        tabContainer.setTabMinWidth(tabWidth);
-        tabContainer.setTabMaxWidth(tabWidth);
-        tabContainer.setTabMinHeight(tabWidth);
-        tabContainer.setTabMaxHeight(tabWidth);
-        tabContainer.setRotateGraphic(true);
-
-        String localDir = System.getProperty("user.dir");
-
-        configureTab(timetableTab, "Stundenplan", new FileInputStream(localDir + "/src/main/resources/at/schoolbooster/img/tab_icon.png"));
-        configureTab(checklistTab, "Checkliste", new FileInputStream(localDir + "/src/main/resources/at/schoolbooster/img/tab_icon.png"));
-        configureTab(datesTab, "Termine", new FileInputStream(localDir + "/src/main/resources/at/schoolbooster/img/tab_icon.png"));
-    }
-
-    @FXML
-    private void configureTab(Tab tab, String title, FileInputStream iconPath) {
-
-        double imageWidth = 50.0;
-
-        ImageView imageView = new ImageView(new Image(iconPath));
-        imageView.setFitHeight(imageWidth);
-        imageView.setFitWidth(imageWidth);
-
-        tab.setText("");
-        tab.setGraphic(imageView);
-    }*/
 }
