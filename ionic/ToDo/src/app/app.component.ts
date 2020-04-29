3e68edc3c31bfd773046aa59dd168e2cd8cf79b0
samuelreichoer <samuelreichoer@users.noreply.github.com>
2020-04-29 17:22:01 +0200

    TodoList

--- ionic/ToDo/src/app/app.component.ts
+++ ionic/ToDo/src/app/app.component.ts
@@ -10,18 +10,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+  
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+  rootPage:any = HomePage;
+
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+
     });
   }
 }
