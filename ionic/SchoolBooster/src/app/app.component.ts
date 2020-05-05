6c514b317608e9fa0adb88a9db6f6878eb4ca851
Niklas Strassl <40146720170043@litec.ac.at>
2020-05-05 23:29:26 +0200

    trying to get the tabs changeable

--- ionic/SchoolBooster/src/app/app.component.ts
+++ ionic/SchoolBooster/src/app/app.component.ts
@@ -1,28 +1,31 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, IonTabs } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+  templateUrl: /*'app.component.html'*/'../index.html',
   styleUrls: ['app.component.scss']
 })
+
 export class AppComponent {
-  isList: boolean;
-    isLocation: boolean;
-    isSelf: boolean;
-    isNotifications: boolean;
-    isCamera: boolean;
-    isSearch: boolean;
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    isChecklist: boolean;
+    isTimetable: boolean;
+    isTutors: boolean;
+    isChat: boolean;
+    isHelp: boolean;
+    isSettings: boolean;
+
+    @ViewChild('tabs') nav: IonTabs;
+
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar
+    ){
+        this.initializeApp();
+    }
 
   initializeApp() {
     this.platform.ready().then(() => {
@@ -32,42 +35,42 @@ export class AppComponent {
   }
 
   togglePage(whichPage: string): void {
-    this.isList=false;
-    this.isLocation=false;
-    this.isSelf=false;
-    this.isNotifications=false;
-    this.isSearch=false;
-    this.isCamera=false;
+    this.isChecklist = true;
+    this.isTimetable = false;
+    this.isTutors = false;
+    this.isChat = false;
+    this.isHelp = false;
+    this.isSettings = false;
   
-    let newTab: String='';
+    let newTab: string='';
   
     switch (whichPage) {
-      case'List':
-        this.isList=true;
-        newTab='PhotosPage';
-        break;
-      case'Location':
-        this.isLocation=true;
-        newTab='LocationsPage';
-        break;
-      case'Self':
-        this.isSelf=true;
-        newTab='SelfPage';
-        break;
-      case'Notifications':
-        this.isNotifications=true;
-        newTab='NotificationsPage';
-        break;
-      case'Search':
-        this.isSearch=true;
-        newTab='SearchPage';
-        break;
-      case'Camera':
-        this.isCamera=true;
-        newTab='CameraPage';
-        break;
+        case'checklist':
+            this.isChecklist = true;
+            newTab='checklist';
+            break;
+        case'timetable':
+            this.isTimetable = true;
+            newTab='timetable';
+            break;
+        case'tutors':
+            this.isTutors = true;
+            newTab='tutuors';
+            break;
+        case'chat':
+            this.isChat = true;
+            newTab='chat';
+            break;
+        case'help':
+            this.isHelp=true;
+            newTab='help';
+            break;
+        case'settings':
+            this.isSettings=true;
+            newTab='settings';
+            break;
      }
   
-     //this.nav.setRoot(newTab);
+     this.nav.select(newTab);
   }
 }
